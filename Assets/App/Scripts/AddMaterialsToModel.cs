16ece8d0bc90493bc888e4d34c3c945acc60ef4c
Naman Bhoj <namanbhoj99@gmail.com>
2024-04-22 13:19:42 -0600

    works with both the ipad

--- Assets/App/Scripts/AddMaterialsToModel.cs
+++ Assets/App/Scripts/AddMaterialsToModel.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class AddMaterialsToModel: MonoBehaviour
 {
 //  public Material Material; 
-public Material wallMaterial ; 
-bool comeHere = true; 
+// public Material wallMaterial ; 
+// bool comeHere = true; 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,37 +17,37 @@ bool comeHere = true;
 
     public void DoorMaterial(){
             
-        //  child.GetComponent<MeshRenderer>().material.color. = 0.25f;
-         GameObject door = GameObject.Find("House_Export1/doorClose");
-         if(door === null)
-            return; 
-        door.GetComponent<MeshRenderer>().material.color = Color.blue;
-        Transform doorP = door.GetComponent<Transform>();
+        // //  child.GetComponent<MeshRenderer>().material.color. = 0.25f;
+        //  GameObject door = GameObject.Find("House_Export1/doorClose");
+        //  if(door === null)
+        //     return; 
+        // door.GetComponent<MeshRenderer>().material.color = Color.blue;
+        // Transform doorP = door.GetComponent<Transform>();
         
-            Vector3 doorPos = new Vector3(doorP.position.x, doorP.position.y, doorP.position.z + 0.13f);
-            comeHere = false; 
-            door.GetComponent<Transform>().position = doorPos;
+        //     Vector3 doorPos = new Vector3(doorP.position.x, doorP.position.y, doorP.position.z + 0.13f);
+        //     comeHere = false; 
+        //     door.GetComponent<Transform>().position = doorPos;
         }
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
 
-        GameObject child = GameObject.Find("House_Export1/wall");
-        child.GetComponent<MeshRenderer>().material = wallMaterial;
+        // GameObject child = GameObject.Find("House_Export1/wall");
+        // child.GetComponent<MeshRenderer>().material = wallMaterial;
         
         // child.GetComponent<MeshRenderer>().material.SetFloat("_Mode", 3);
         
         // color.a = 0.25f; 
        
         // child.GetComponent<MeshRenderer>().material.color = color;
-        if(comeHere == true){
-            DoorMaterial(); 
-        }
+        // if(comeHere == true){
+        //     DoorMaterial(); 
+        // }
     
         
-        Debug.Log("***************************************" + child.name);
+        // Debug.Log("***************************************" + child.name);
         
         
         //child.GetComponent<Renderer>().material = Resourses.Load("green", typeof(Material)) as Material; 
